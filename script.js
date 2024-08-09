2aa00ccfbca55d2020885d7c8b4c923b21d5dc35
Shanecode3 <166157549+Shanecode3@users.noreply.github.com>
2024-08-09 15:01:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function fn() {
+    const inp = document.getElementById("boop").value;
+    // console.log("hello");
+    // alert(inp);
+    window.location.assign("https://mail.google.com/mail/u/1/#inbox?compose=CllgCJfrLJwxmfdKsgtFxwXtwQNCZfTZRFZNXSxGgRQlQhmKltRjKPrLqCmcNWMXhfWFwzRjdwL");
+}
